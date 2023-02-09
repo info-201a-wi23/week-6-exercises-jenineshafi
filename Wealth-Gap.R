be8b429f62d64a16614c00b9b362bd6c6a1a6426
jenineshafi <jenineshafi@gmail.com>
2023-02-09 12:10:14 -0800

    line plot

--- Wealth-Gap.R
+++ Wealth-Gap.R
@@ -1,11 +1,11 @@
 # Analyzing Family Wealth & Home Ownership Over Time in the U.S.
 
 home_owner <- read.csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/home_owner.csv')
-
 race_wealth <- read.csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-02-09/race_wealth.csv')
 
 # Load relevant libraries
-Your code here
+library("ggplot2")
+library("dplyr")
 
 # How is average family wealth in the U.S. trending over time?
 
@@ -13,27 +13,37 @@ Your code here
 # Then we need to make a new dataframe from race_wealth with the average family wealth per year for all races
 # Save this as `total_avg_wealth`
 
-Your code here
-
+total_avg_wealth <- race_wealth %>%
+                    filter(type == "Average") %>% 
+                    group_by(year) %>% 
+                    summarize(average = mean(wealth_family, na.rm = TRUE))
+                              
 # Now let's make a scatter plot of total average family wealth over time
 
-Your code here
+ggplot(data = total_avg_wealth) + 
+  geom_point(mapping = aes(x = year, y = average))
 
 # Now let's draw a line plot on top of the same scatterplot
 
-Your code here
+ggplot(data = total_avg_wealth) + 
+  geom_point(mapping = aes(x = year, y = average)) +
+  geom_line(mapping = aes(x = year, y = average))
 
 # Let's plot family wealth over time by racial category
 
 # Use the race_wealth dataframe and filter for only "Average" wealth `avg_race_wealth`
 
-Your code here
+avg_race_wealth <- race_wealth %>% 
+                   filter(type == "Average")
 
 # Let's make a line plot of average family wealth over time by race
 
-Your code here
+ggplot(data = avg_race_wealth) + 
+  geom_point(mapping = aes(x = year, y = wealth_family, color = race)) +
+  geom_line(mapping = aes(x = year, y = wealth_family, color = race))
 
 # Finally, let's make a line plot of the percentage of homeowners for racial categories over time, and color by race
 # Use the home_owner dataframe
 
-Your code here
+ggplot(data = home_owner) + 
+  geom_line(mapping = aes(x = year, y = home_owner_pct, color = race))
